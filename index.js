92903839cba7c17584e16426f757cfaa28759ce7
Dylan Baker <dylanmbaker@yahoo.com>
2021-11-02 14:13:08 -0500

    finished most of the problems

--- index.js
+++ index.js
@@ -158,21 +158,31 @@ RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors
 HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
 
+let randomNumber = Math.round(Math.random() * 2);
+// console.log(randomNumber);
+
+function computersChoice(i){
+  if (i === 0){
+    return 'rock';
+  } else if (i === 1){
+    return 'paper';
+  } else if (i === 2){
+    return 'scissors';
+  }
+}
+// console.log(computersChoice(randomNumber));
+
 function game(user, computer){
-  if(user > computer){
+  if(user === 'rock' && computer === 'scissors' || user === 'paper' && computer === 'rock' || user === 'scissors' && computer === 'paper'){
     return "you win!";
-  }else if(user < computer){
-    return "you lose!";
-  }else if(user === computer){
+  } else if (user === computer){
     return "it's a tie";
+  } else {
+    return "you lose!";
   }
 }
 
-let random = Math.random() * 1;
-
-console.log("Task 4:", game(0.5, random))
-
-console.log(random);
+console.log('Task 4', game(1, randomNumber))
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -187,11 +197,10 @@ Using the miles function below do the following:
 */
 
 function miles(km){
-  /*add your code here*/
-  return km / 1.609
+  return km * 0.621371;
 }
 
-console.log('Task 5:', miles(2))
+console.log('Task 5:', miles(2));
 
 
 //Task 5b - Feet to CM
@@ -202,10 +211,11 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48;
 }
 
+console.log('Task 5b:', feet(2));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -218,10 +228,11 @@ Using the annoyingSong function below do the following:
     "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(){
+
 }
 
+console.log('Task 6:')
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -238,9 +249,21 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+  if(score >= 90 && score <= 100){
+    return 'you got an A';
+  }else if(score >= 80 && score <= 89){
+    return 'you got a B';
+  }else if(score >= 70 && score <= 79){
+    return 'you got a C';
+  }else if(score >= 60 && score <= 69){
+    return 'you got a D';
+  }else if(score <= 59){
+    return 'you got an F';
+  }
 }
+
+console.log('Task 7:', grade(90));
 
 
 
